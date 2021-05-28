0b20a4fd08012ac538c183c98505577b8ee53cac
Colderry <72723052+Colderry@users.noreply.github.com>
2021-05-28 12:08:52 +0600

    Error: index

---
+++ src/error/index.ts
@@ -0,0 +1,6 @@
+const errors = {
+    Message_Missing: "No specify message\nNeed support? Join https://discord.gg/QSN7tCPrHd",
+    Bad_Auth: "Bad auth authentication failed\nNeed support? Join https://discord.gg/QSN7tCPrHd"
+}
+
+export default errors;
